41fc7618fe7830ad21b672e5749676c4699158ec
catpea <corporate.miou@gmail.com>
2021-04-11 20:51:55 -0400

    API Cleanup

--- api.mjs
+++ api.mjs
@@ -1,21 +1,19 @@
 import fs from 'fs';
 import path from 'path';
-
 import { mkdir, writeFile, copyFile } from 'fs/promises';
 
 export default { jsonParse, createIndex, createDirectories, importFiles, };
 
-
 async function createIndex(so){
-    const baseDirectory = path.resolve( path.join('.', so.name) );
-    console.log(baseDirectory);
-    await mkdir(baseDirectory, {recursive: true});
-    const head = Object.keys(so).filter(i=>(i !== 'data')).map(i=>[i, so[i]]);
-    const data = so.data.map(i=>i.id);
-    head.push(['data', data])
-    const index = Object.fromEntries(head)
-    const indexFileLocation = path.join(baseDirectory, 'index.json');
-    await writeFile(indexFileLocation, JSON.stringify(index, null, '  '))
+  const baseDirectory = path.resolve( path.join('.', so.name) );
+  console.log(baseDirectory);
+  await mkdir(baseDirectory, {recursive: true});
+  const head = Object.keys(so).filter(i=>(i !== 'data')).map(i=>[i, so[i]]);
+  const data = so.data.map(i=>i.id);
+  head.push(['data', data])
+  const index = Object.fromEntries(head)
+  const indexFileLocation = path.join(baseDirectory, 'index.json');
+  await writeFile(indexFileLocation, JSON.stringify(index, null, '  '))
 }
 
 async function importFiles(so, webDir, audioDir, imageDir){
@@ -23,7 +21,6 @@ async function importFiles(so, webDir, audioDir, imageDir){
   for(const item of so.data){
     const filesDirectory = path.join(baseDirectory, item.id, 'files');
     await mkdir(filesDirectory, {recursive: true});
-
     if(audioDir){
       if(item.audio){
         const sourceFile = path.join(audioDir, item.audio);
@@ -37,9 +34,7 @@ async function importFiles(so, webDir, audioDir, imageDir){
         console.warn( `WARN: ${item.audio} was not imported because audioDir was not specified`);
       }
     }
-
     if(imageDir){
-
       if(item.image){
         const sourceFile = path.join(imageDir, item.image);
         const destinationFile = path.join(filesDirectory, item.image);
@@ -47,7 +42,6 @@ async function importFiles(so, webDir, audioDir, imageDir){
           copyFile( sourceFile, destinationFile);
         }
       }
-
       if(item.images){
         for(const image of item.images){
           const sourceFile = path.join(imageDir, image.url);
@@ -57,7 +51,6 @@ async function importFiles(so, webDir, audioDir, imageDir){
           }
         }
       }
-
     }else{
       if(item.image){
         console.warn( `WARN: ${item.image} was not imported because imageDir was not specified`);
@@ -68,7 +61,6 @@ async function importFiles(so, webDir, audioDir, imageDir){
         }
       }
     }
-
     if(webDir){
       if(item.links){
         for(const link of item.links){
@@ -90,46 +82,28 @@ async function importFiles(so, webDir, audioDir, imageDir){
         }
       }
     }
-
   }
-
 }
 
 async function createDirectories(so){
-    const baseDirectory = path.resolve( path.join('.', so.name) );
-
-
-    for(const item of so.data){
-
-      // pre-create directories
-      const dataDirectory = path.join(baseDirectory, item.id);
-      const filesDirectory = path.join(baseDirectory, item.id, 'files');
-      const cacheDirectory = path.join(baseDirectory, item.id, 'cache');
-      await mkdir(dataDirectory, {recursive: true});
-      await mkdir(filesDirectory, {recursive: true});
-      await mkdir(cacheDirectory, {recursive: true});
-
-      // create configuration
-
-      const dynamicFields = ['text', 'images', 'links', 'print'];
-      const requiredFields = ['title', 'date', 'image', 'audio', 'id'];
-
-      const configuration = Object.fromEntries(Object.keys(item).filter(i=>requiredFields.includes(i)).map(i=>[i, item[i]]));
-      const configurationFileLocation = path.join(dataDirectory, 'configuration.json');
-      await writeFile(configurationFileLocation, JSON.stringify(configuration, null, '  '))
-
-      const contentFileLocation = path.join(dataDirectory, 'content.html');
-      // TODO: MAYBE instead of using <p> to delimit the line couplets use a <br>, or just leave the old alone.
-      await writeFile(contentFileLocation, item.html)
-
-
-
-
-
-
-      // TODO: create cache folder (maybe in the re-compiler)
-
-    }
+  const baseDirectory = path.resolve( path.join('.', so.name) );
+  for(const item of so.data){
+    // pre-create directories
+    const dataDirectory = path.join(baseDirectory, item.id);
+    const filesDirectory = path.join(baseDirectory, item.id, 'files');
+    const cacheDirectory = path.join(baseDirectory, item.id, 'cache');
+    await mkdir(dataDirectory, {recursive: true});
+    await mkdir(filesDirectory, {recursive: true});
+    await mkdir(cacheDirectory, {recursive: true});
+    // create configuration
+    const dynamicFields = ['text', 'images', 'links', 'print'];
+    const requiredFields = ['title', 'date', 'image', 'audio', 'id'];
+    const configuration = Object.fromEntries(Object.keys(item).filter(i=>requiredFields.includes(i)).map(i=>[i, item[i]]));
+    const configurationFileLocation = path.join(dataDirectory, 'configuration.json');
+    await writeFile(configurationFileLocation, JSON.stringify(configuration, null, '  '))
+    const contentFileLocation = path.join(dataDirectory, 'content.html');
+    await writeFile(contentFileLocation, item.html)
+  }
 }
 
 async function jsonParse(target){
